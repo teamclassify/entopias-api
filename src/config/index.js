a756f283619855d20617f3ab53e34dcab2cf58fd
Andres Parra <byandrev@gmail.com>
2025-03-17 19:58:09 -0500

    chore: config prisma

--- src/config/index.js
+++ src/config/index.js
@@ -7,4 +7,20 @@ const PORT = process.env.PORT || 8080;
 const MODE = process.env.MODE || "dev";
 const URL_FRONT = process.env.URL_FRONT;
 
-export { MODE, PAGINATION_LIMIT, PORT, URL_FRONT };
+const FIREBASE = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY
+    ? process.env.PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.CLIENT_CERT,
+  universe_domain: "googleapis.com",
+};
+
+export { MODE, PAGINATION_LIMIT, PORT, URL_FRONT, FIREBASE };
