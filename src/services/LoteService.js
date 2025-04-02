19ff65ff9c3f298c38a757e37d6c6a4b17782838
Andres Parra <byandrev@gmail.com>
2025-04-02 17:26:53 -0500

    feat: add pagination to findAll method in LoteService

--- src/services/LoteService.js
+++ src/services/LoteService.js
@@ -1,48 +1,56 @@
 import prisma from "../config/prisma.js";
 
 class LoteService {
-    constructor() { }
-    async create(data) {
-        return await prisma.lote.create({
-            data,
-            include: { cafe: true },
-        });
-    }
+  constructor() {}
+  async create(data) {
+    return await prisma.lote.create({
+      data,
+      include: { cafe: true },
+    });
+  }
 
-    async find(where) {
-        const cafes = await prisma.cafe.findMany({
-            where,
-            include: { cafe: true }
-        });
-        return cafes;
-    }
+  async find(where) {
+    const cafes = await prisma.cafe.findMany({
+      where,
+      include: { cafe: true },
+    });
+    return cafes;
+  }
 
-    async findAll() {
-        return await prisma.lote.findMany({
-            include: { cafe: true },
-        });
-    }
+  async findAll({ page = 1, where }) {
+    return await prisma.lote.findMany({
+      where,
 
-    async findOne(id) {
-        return await prisma.lote.findUnique({
-            where: { id },
-            include: { cafe: true },
-        });
-    }
+      include: { cafe: true },
 
-    async update(id, data) {
-        return await prisma.lote.update({
-            where: { id },
-            data,
-            include: { cafe: true },
-        });
-    }
+      skip: (page - 1) * 10,
+      take: 10,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
 
-    async delete(id) {
-        return await prisma.lote.delete({
-            where: { id },
-        });
-    }
+  async findOne(id) {
+    return await prisma.lote.findUnique({
+      where: { id },
+      include: { cafe: true },
+    });
+  }
+
+  async update(id, data) {
+    return await prisma.lote.update({
+      where: { id },
+      data,
+      include: { cafe: true },
+    });
+  }
+
+  async delete(id) {
+    return await prisma.lote.delete({
+      where: { id },
+    });
+  }
 }
 
 export default LoteService;
