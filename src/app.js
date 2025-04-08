44e86d1f422375f8533bef4f44413dba18c713be
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-04-08 09:42:30 -0500

    fix: update routes imports

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ import handleErrors from "./middlewares/handleErrors.js";
 
 import authRouter from "./routes/Auth.js";
 import cafeRouter from "./routes/Cafes.js";
-import lotesRouter from "./routes/Lotes.js";
 import productsRouter from "./routes/Products.js";
 import roleRoutes from "./routes/RoleRoutes.js";
 import userRoutes from "./routes/UserRoutes.js";
+import batchRouter from "./routes/Batches.js";
+import VarietyRouter from "./routes/Varieties.js";
+import producerRouter from "./routes/Producer.js";
 
 const app = express();
 
@@ -30,8 +32,9 @@ app.use("/api/auth", authRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/users", userRoutes);
 app.use("/api/roles", roleRoutes);
-app.use("/api/cafes", cafeRouter);
-app.use("/api/lotes", lotesRouter);
+app.use("/api/lotes", batchRouter);
+app.use("/api/variedades", VarietyRouter);
+app.use("/api/productores", producerRouter);
 
 app.use(handleErrors);
 
