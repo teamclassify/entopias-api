37cc11d3402bf659ead7a881785735b986fbdf94
Andres Parra <byandrev@gmail.com>
2025-04-21 17:38:59 -0500

    feat: implement OrderService for managing order data with methods to count, find, and retrieve individual orders

---
+++ src/services/OrderService.js
@@ -0,0 +1,61 @@
+import prisma from "../config/prisma.js";
+
+class OrderService {
+  constructor() {}
+
+  async countAll({ where } = {}) {
+    const count = await prisma.order.count({
+      where,
+    });
+
+    return count;
+  }
+
+  async find({ page = 1, where }) {
+    const orders = await prisma.order.findMany({
+      where,
+      include: {
+        user: true,
+        items: {
+          include: {
+            variety: {
+              include: {
+                product: true,
+              },
+            },
+          },
+        },
+      },
+
+      skip: (page - 1) * 10,
+      take: 10,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return orders;
+  }
+
+  async findOne({ id }) {
+    const order = await prisma.order.findUnique({
+      where: { id },
+      include: {
+        user: true,
+        items: {
+          include: {
+            variety: {
+              include: {
+                product: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return order;
+  }
+}
+
+export default OrderService;
