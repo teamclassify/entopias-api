b937c38764aba7c5b3745f597f4a5d3091ba1bf3
Andres Parra <byandrev@gmail.com>
2025-04-21 16:22:59 -0500

    feat: implement addProductToCart method in CartController for adding products to the user's cart

--- src/controllers/CartController.js
+++ src/controllers/CartController.js
@@ -1,4 +1,5 @@
 import CartService from "../services/CartService.js";
+import ResponseDataBuilder from "../models/ResponseData.js";
 
 class CartController {
   constructor() {
@@ -17,6 +18,29 @@ class CartController {
       .build();
 
     res.status(data.status).json(data);
+  };
+
+  addProductToCart = async (req, res, next) => {
+    const id = req.id;
+    const { varietyId, quantity } = req.body;
+
+    try {
+      const cart = await this.cartService.addProductToCart({
+        userId: id,
+        varietyId,
+        quantity,
+      });
+
+      const data = new ResponseDataBuilder()
+        .setData(cart)
+        .setStatus(200)
+        .setMsg("Product added to cart")
+        .build();
+
+      res.status(data.status).json(data);
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
